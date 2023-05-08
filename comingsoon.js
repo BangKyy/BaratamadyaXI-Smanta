6f204b13749f2a1f639cc47d51f93958a7f299d5
unknown <xenemies.27@gmail.com>
2023-05-09 06:20:47 +0700

    Remove coming soon section and change main section page

--- comingsoon.js
+++ comingsoon.js
@@ -4,7 +4,7 @@ function countdownDate () {
         hour = minute * 60,
         day = hour * 24;
 
-    let countDown = new Date('May 10, 2023 00:00:00').getTime(),
+    let countDown = new Date('May 11, 2023 00:00:00').getTime(),
         x = setInterval(function() {    
 
         let now = new Date().getTime(),
@@ -14,6 +14,17 @@ function countdownDate () {
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+        
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("days").innerHTML = "00"
+            document.getElementById("hours").innerHTML = "00" 
+            document.getElementById("minutes").innerHTML = "00"
+            document.getElementById("seconds").innerHTML = "00"
+            document.querySelector(".time-text").innerHTML = "Harap Tunggu!!";
+            document.querySelector(".small-heading").innerHTML = "SMAN 01 Talun";
+            document.querySelector(".heading").innerHTML = "PRAJA WIRA";
+        }
     }, second)
 }
 countdownDate();
